244581397fe4fcf0329c9d45f5fd3a10bd520ecb
thitiwat-buatip <subin2538@gmail.com>
2017-02-01 16:29:27 +0700

    Initial Commit

---
+++ Lab 1/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Lab 1
+//
+//  Created by thitiwat on 1/2/60.
+//  Copyright © พ.ศ. 2560 thitiwat. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Lab 1/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Lab 1
+//
+//  Created by thitiwat on 1/2/60.
+//  Copyright © พ.ศ. 2560 thitiwat. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
